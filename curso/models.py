69a5e9d82db1412a3e04aab6823e4680029703ba
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-05-08 00:04:40 -0600

    [REMOVE] Elimina los campos 'foto' y 'foto_binaria' del modelo 'Curso', actualiza el serializador 'CursoSerializer' y ajusta el archivo 'requirements.txt' para reflejar estos cambios.

--- curso/models.py
+++ curso/models.py
@@ -1,4 +1,3 @@
-import base64
 from django.db import models
 
 
@@ -18,12 +17,6 @@ class Curso(models.Model):
         default='ABIERTO',
     )
     
-    # CAMPO PARA GUARDAR LA IMAGEN EN CARPETA MEDIA
-    foto = models.ImageField(upload_to='curso2/', blank=True, null=True)
-
-    # CAMPO BINARIO PARA LA IMAGEN EN LA BD
-    foto_binaria = models.BinaryField(blank=True, null=True)
-
     def abrir(self):
         self.estado = 'ABIERTO'
         self.save()
@@ -34,10 +27,3 @@ class Curso(models.Model):
 
     def __str__(self):
         return f"{self.nombre} ({self.codigo})"
-    
-    @property
-    def foto_base64_display(self):
-        if self.foto_binaria:
-            codificado = base64.b64encode(self.foto_binaria).decode('utf-8')
-            return f"data:image/png;base64,{codificado}"
-        return None
--- curso/serializers.py
+++ curso/serializers.py
@@ -3,41 +3,6 @@ from .models import Curso
 
 
 class CursoSerializer(serializers.ModelSerializer):
-    # Campo para recibir la imagen que se guardará como binario
-    foto_para_binario = serializers.ImageField(write_only=True, required=False)
-    # Campo para mostrar la imagen en base64
-    foto_base64_display = serializers.ReadOnlyField()
-
     class Meta:
         model = Curso
-        fields = ['id', 'nombre', 'codigo', 'creditos', 'profesor', 'cupo_maximo', 'estado',
-             'foto', 'foto_para_binario', 'foto_base64_display']
-    
-    def create(self, validated_data):
-        # Extraemos el archivo para binario
-        archivo_binario = validated_data.pop('foto_para_binario', None)
-
-        # DRF guarda 'foto' automáticamente en la carpeta /media/
-        curso = Curso.objects.create(**validated_data)
-
-        # Si mandaron el archivo binario, guardamos los bytes
-        if archivo_binario:
-            curso.foto_binaria = archivo_binario.read()
-            curso.save()
-
-        return curso
-    
-    def update(self, instance, validated_data):
-        # Extraemos el archivo para binario
-        archivo_binario = validated_data.pop('foto_para_binario', None)
-
-        # Actualizamos los campos normales
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        # Si mandaron el archivo binario, guardamos los bytes
-        if archivo_binario:
-            instance.foto_binaria = archivo_binario.read()
-
-        instance.save()
-        return instance
+        fields = ['id', 'nombre', 'codigo', 'creditos', 'profesor', 'cupo_maximo', 'estado']
