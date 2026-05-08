da8ee56739fdb258baa5b14c7221d5f45afc82ec
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-05-07 23:27:47 -0600

    [ADD] Implementa el modelo 'MiUsuario' y su administrador en Django, incluyendo gestión de usuarios y superusuarios con campos personalizados.

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MiUsuario
 
-# Register your models here.
+@admin.register(MiUsuario)
+class MiUsuarioAdmin(UserAdmin):
+    list_display = ('email', 'nombre_completo', 'telefono', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    search_fields = ('email', 'nombre_completo')
+    ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Información Personal', {'fields': ('nombre_completo', 'telefono')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas Importantes', {'fields': ('last_login',)}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nombre_completo', 'password1', 'password2', 'telefono'),
+        }),
+    )
