f2aa09d68880292fa327872810630848e69f7163
Nadia Yuridia Salinas Cervantes <20233tn237@utez.edu.mx>
2026-05-07 23:25:40 -0600

    [ADD] Añade configuración de URLs para la aplicación 'curso', incluyendo el enrutador para el conjunto de vistas 'CursoViewSet'.

---
+++ curso/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CursoViewSet
+
+router = DefaultRouter()
+router.register(r'', CursoViewSet, basename='curso2')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+
+
